3444cefa411f1964be20705af95aceac605d9a2d
Sharp0802 <nrs070802@gmail.com>
2021-05-30 02:18:34 +0900

    Change asm info

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyProduct("WebP.Net")]
+[assembly: ComVisible(true)]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.*")]
