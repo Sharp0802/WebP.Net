ab5b313efb513226a96acd3f952f4f7474b9563d
Sharp0802 <nrs070802@gmail.com>
2021-07-09 19:58:09 +0900

    Code refactoring

--- Enums/WebPCspMode.cs
+++ Enums/WebPCspMode.cs
@@ -1,22 +1,23 @@
-﻿using System.Diagnostics.CodeAnalysis;
-
-namespace WebP.Net.Enums
+﻿namespace WebP.Net.Enums
 {
-	[SuppressMessage("ReSharper", "InconsistentNaming")]
 	public enum WebpCspMode
 	{
-		ModeRGB,
+		ModeRgb,
 		ModeRgba,
 		ModeBgr,
 		ModeBgra,
+		
 		ModeARGB,
 		ModeRGBA4444,
 		ModeRGB565,
+		
 		ModeRgbA,
 		ModeBgrA,
+		
 		ModeArgb,
 		ModeRgbA4444,
 		ModeYuv,
+		
 		ModeYuva,
 		ModeLast,
 	}
--- Helpers/ThrowHelper.cs
+++ Helpers/ThrowHelper.cs
@@ -8,33 +8,33 @@ namespace WebP.Net.Helpers
 {
 	internal static class ThrowHelper
 	{
-		[Pure, DoesNotReturn]
+		[DoesNotReturn]
 		public static Exception Create(
-			Exception                 inner,
-			[CallerMemberName] string caller = "Unknown")
+			[DisallowNull]     Exception inner,
+			[CallerMemberName] string    caller = "Unknown")
 			=> new($"{inner.Message}\nIn {caller}", inner);
 
-		[Pure, DoesNotReturn]
+		[DoesNotReturn]
 		public static Exception UnknownPlatform([CallerMemberName] string caller = "Unknown")
 			=> Create(new PlatformNotSupportedException("Unknown platform detected. Platform must be x86 or x64"), caller);
 
-		[Pure, DoesNotReturn]
+		[DoesNotReturn]
 		public static Exception ContainsNoData([CallerMemberName] string caller = "Unknown")
-			=> Create(new DataException("Bitmap contains no data"));
+			=> Create(new DataException("Bitmap contains no data"), caller);
 
-		[Pure, DoesNotReturn]
+		[DoesNotReturn]
 		public static Exception SizeTooBig([CallerMemberName] string caller = "Unknown")
 			=> Create(new DataException($"Dimension of bitmap is too large. Max is {WebPEncoder.WebpMaxDimension}x{WebPEncoder.WebpMaxDimension} pixels"), caller);
 
-		[Pure, DoesNotReturn]
+		[DoesNotReturn]
 		public static Exception CannotEncodeByUnknown([CallerMemberName] string caller = "Unknown")
 			=> Create(new Exception("Cannot encode by unknown cause"), caller);
 
-		[Pure, DoesNotReturn]
+		[DoesNotReturn]
 		public static Exception NullReferenced(string var, [CallerMemberName] string caller = "Unknown")
 			=> Create(new NullReferenceException($"{var} is null"), caller);
 
-		[Pure, DoesNotReturn]
+		[DoesNotReturn]
 		public static Exception QualityOutOfRange([CallerMemberName] string caller = "Unknown")
 			=> Create(new NullReferenceException("Quality must be between"), caller);
 	}
