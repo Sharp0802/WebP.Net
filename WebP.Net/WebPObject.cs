2fdcc8745ff002db60742130417a3282f90482b7
Sharp0802 <nrs070802@gmail.com>
2021-08-29 22:11:00 +0900

    Impl methods

--- WebP.Net/WebPObject.cs
+++ WebP.Net/WebPObject.cs
@@ -10,32 +10,39 @@ using WebP.Net.Natives.Structs;
 
 namespace WebP.Net
 {
-	public class WebPObject : IDisposable
+	public sealed class WebPObject : IDisposable
 	{
-		private const int WebpMaxDimension = 16383;
-
-		private (IntPtr Pointer, int Size) DynamicArray { get; set; }
+		private (IntPtr Pointer, int Size) DynamicArray { get; set; } = (IntPtr.Zero, 0);
 
 		private object CacheLockHandle { get; } = new();
 
 		private byte[] _bytesCache;
 
-		protected byte[] BytesCache
+		private byte[] BytesCache
 		{
 			get
 			{
-				if (DynamicArray.Pointer == IntPtr.Zero) return null;
-
 				lock (CacheLockHandle)
 				{
 					if (_bytesCache is not null)
 						return _bytesCache;
+					if (DynamicArray.Pointer == IntPtr.Zero)
+						return null;
 					_bytesCache = new byte[DynamicArray.Size];
 					Marshal.Copy(DynamicArray.Pointer, _bytesCache, 0, DynamicArray.Size);
 					return _bytesCache;
 				}
 			}
+			set => _bytesCache = value;
 		}
+
+		private Image ImageCache { get; set; }
+
+		private WebPInfo? InfoCache { get; set; }
+
+		private const int WebpMaxDimension = 16383;
+		
+		#region Private methods
 
 		[method: MethodImpl(MethodImplOptions.AggressiveInlining)]
 		private static void VerifyImage(Image image)
@@ -61,24 +68,34 @@ namespace WebP.Net
 		}
 
 		[method: MethodImpl(MethodImplOptions.AggressiveInlining)]
-		private static BitmapData GetData(Bitmap bitmap, ImageLockMode mode)
+		private static BitmapData GetBitmapData(Bitmap bitmap, ImageLockMode mode)
 		{
 			return bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), mode, bitmap.PixelFormat);
 		}
 
 		[method: MethodImpl(MethodImplOptions.AggressiveInlining)]
-		protected void Encode(Image image, bool lossy, float quality)
+		private static WebPInfo GetInfoFrom(IntPtr pointer, int size)
+		{
+			var features = new WebPBitstreamFeatures();
+			var status   = Native.WebPGetFeatures(pointer, size, ref features);
+			if (status is not Vp8StatusCode.Ok)
+				throw new ExternalException(status.ToString());
+			return new WebPInfo(features);
+		}
+
+		[method: MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private void StoreEncodedResult(Image image, bool lossy, float quality)
 		{
 			VerifyImage(image);
 			using var  bitmap = ConvertTo32Argb(image);
 			BitmapData data   = null;
 			try
 			{
-				data = GetData(bitmap, ImageLockMode.ReadOnly);
+				data = GetBitmapData(bitmap, ImageLockMode.ReadOnly);
 				var size = lossy
 					? Native.WebPEncodeBgra(data.Scan0, data.Width, data.Height, data.Stride, quality, out var ptr)
 					: Native.WebPEncodeLosslessBgra(data.Scan0, data.Width, data.Height, data.Stride, out ptr);
-				if (size is 0) 
+				if (size is 0)
 					throw ThrowHelper.CannotEncodeByUnknown();
 				DynamicArray = (ptr, size);
 			}
@@ -88,20 +105,10 @@ namespace WebP.Net
 			}
 		}
 
-		[method: MethodImpl(MethodImplOptions.AggressiveInlining)]
-		private static WebPInfo GetFrom(IntPtr pointer, int size)
-		{
-			var features = new WebPBitstreamFeatures();
-			var status   = Native.WebPGetFeatures(pointer, size, ref features);
-			if (status is not Vp8StatusCode.Ok)
-				throw new ExternalException(status.ToString());
-			return new WebPInfo(features);
-		}
-
 		private delegate int DecodeInto(IntPtr ptr, int size, IntPtr output, int outputSize, int outputStride);
 
 		[method: MethodImpl(MethodImplOptions.AggressiveInlining)]
-		protected static Bitmap Decode(IntPtr pointer, int size)
+		private static Bitmap Decode(IntPtr pointer, int size)
 		{
 			Bitmap     bmp  = null;
 			BitmapData data = null;
@@ -109,11 +116,11 @@ namespace WebP.Net
 
 			try
 			{
-				var info = GetFrom(pointer, size);
+				var info = GetInfoFrom(pointer, size);
 				bmp = new Bitmap(info.Width, info.Height, info.HasAlpha
 					                 ? PixelFormat.Format32bppArgb
 					                 : PixelFormat.Format24bppRgb);
-				data = GetData(bmp, ImageLockMode.WriteOnly);
+				data = GetBitmapData(bmp, ImageLockMode.WriteOnly);
 				length = ((DecodeInto) (info.HasAlpha
 						? Native.WebPDecodeBgraInto
 						: Native.WebPDecodeBgrInto))
@@ -129,22 +136,81 @@ namespace WebP.Net
 
 			return bmp;
 		}
+		
+		#endregion
+		
+		#region Public methods
 
 		public Image GetImage()
 		{
+			if (DynamicArray.Pointer != IntPtr.Zero)
+				return ImageCache ??= Decode(DynamicArray.Pointer, DynamicArray.Size);
+			
+			var cache  = BytesCache;
+			var handle = GCHandle.Alloc(cache, GCHandleType.Pinned);
+			try
+			{
+				return ImageCache ??= Decode(handle.AddrOfPinnedObject(), cache.Length);
+			}
+			finally
+			{
+				if (handle.IsAllocated) handle.Free();
+			}
 		}
 
-		public byte[] GetWebP(float quality = 70)
+		public byte[] GetWebPLossy(float quality = 70, bool forceLossy = false)
 		{
+			if (BytesCache is null)
+				StoreEncodedResult(ImageCache, true, quality);
+			else if (forceLossy)
+				StoreEncodedResult(GetImage(), true, quality);
+			return BytesCache;
+		}
+
+		public byte[] GetWebPLossless()
+		{
+			if (BytesCache is null)
+				StoreEncodedResult(ImageCache, false, 0);
+			return BytesCache;
 		}
 
 		public WebPInfo GetInfo()
 		{
+			if (InfoCache.HasValue)
+				return InfoCache.Value;
+			if (DynamicArray.Pointer != IntPtr.Zero)
+				return InfoCache ??= GetInfoFrom(DynamicArray.Pointer, DynamicArray.Size);
+			
+			var cache  = BytesCache;
+			var handle = GCHandle.Alloc(cache, GCHandleType.Pinned);
+			try
+			{
+				return InfoCache ??= GetInfoFrom(handle.AddrOfPinnedObject(), cache.Length);
+			}
+			finally
+			{
+				if (handle.IsAllocated) handle.Free();
+			}
 		}
+		
+		public static WebPVersion GetVersion()
+		{
+			try
+			{
+				var v = (uint) Native.WebPGetDecoderVersion();
+				return new WebPVersion((v >> 16) % 256, (v >> 8) % 256, v % 256);
+			}
+			catch (Exception ex)
+			{
+				throw ThrowHelper.Create(ex);
+			}
+		}
+		
+		#endregion
 
 		~WebPObject() => Dispose();
 
-		public virtual void Dispose()
+		public void Dispose()
 		{
 			if (DynamicArray.Pointer != IntPtr.Zero)
 				Native.WebPFree(DynamicArray.Pointer);
