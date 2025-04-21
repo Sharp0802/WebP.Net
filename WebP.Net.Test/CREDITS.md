94bca7da8fd7d98b8a4ac40cc93fc321bd036c65
Sharp0802 <sharp0802.business@gmail.com>
2025-04-21 11:32:37 +0900

    :white_check_mark: Add test with sample image

---
+++ WebP.Net.Test/CREDITS.md
@@ -0,0 +1,7 @@
+## Credits
+
+- sample.webp
+  
+Image from file-examples.com
+
+https://file-examples.com/index.php/sample-images-download/sample-webp-download/
--- WebP.Net.Test/Program.cs
+++ WebP.Net.Test/Program.cs
@@ -1,7 +1,16 @@
 
 using System;
+using System.Drawing.Imaging;
+using System.IO;
 using WebP.Net;
 
 Console.WriteLine("Hello, World!");
 
 Console.WriteLine("libwebp version: {0}", WebPLibrary.GetVersion().ToString());
+
+var bitmap = WebPDecoder.Decode(File.ReadAllBytes("sample.webp"));
+
+bitmap.Save("sample.png", ImageFormat.Png);
+
+var bytes = WebPEncoder.EncodeLossless(bitmap);
+File.WriteAllBytes("after.webp", bytes);
